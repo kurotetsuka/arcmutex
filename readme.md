e58865eced232c9135a3a875d2cee091f0637082
kurotetsuka <kurotetsuka@gmail.com>
2015-03-27 21:40:05 -0400

    tiny tweaks and comments

--- readme.md
+++ readme.md
@@ -7,7 +7,7 @@ License: [MIT](
 	https://github.com/kurotetsuka/hyphaelia/blob/master/legal/mit.md)
 
 ### Example
-```
+```rust
 let a : ArcMutex< u8> = arcmutex!( 0);
 let mut b = a.lock().unwrap();
 *b += 1;
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
 use std::sync::{ Arc, Mutex};
 
+
 pub type ArcMutex<T> = Arc< Mutex<T>>;
+
+/// Macro for creating an `ArcMutex`.
+///
+/// # Examples
+/// ```
+/// let a : ArcMutex< u8> = arcmutex!( 0);
+/// let mut b = a.lock().unwrap();
+/// *b += 1;
+/// println!( "a: {}", *b);
+/// ```
 
 #[macro_export]
 macro_rules! arcmutex {
